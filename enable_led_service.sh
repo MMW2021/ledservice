e9c2807b831cfc7903fbfdb336d84f96ee17415e
Liam <liamfpower@gmail.com>
2021-12-05 05:51:21 +1100

    changed stuff

--- enable_led_service.sh
+++ enable_led_service.sh
@@ -1,19 +1,12 @@
 #!/bin/bash
 
-#copy service file to audio device
+#copy service file to systemd
 sudo cp pyLEDs.service /etc/systemd/system/
-
-
-sudo cp 10-usb-audio.rules /etc/udev/rules.d/10-usb-audio.rules
-
-
-sudo rm /etc/systemd/pysigaudio.service
-
 
 # enable the service
 sudo systemctl enable pyLEDs.service
 sudo systemctl start pyLEDs.service
 
 # view logs for errors. if there are, change .service files and run
-# sudo sh enable_audio_service.sh && sudo sh restart_audio_service.sh
+# sudo sh enable_led_service.sh 
 journalctl -u pyLEDs -f
